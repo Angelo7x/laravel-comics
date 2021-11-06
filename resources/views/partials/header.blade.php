7edb68578ba72569f4bcbad6b9ce9880bace52bd
Angelo7x <abennici96@gmail.com>
2021-11-06 17:21:27 +0100

    Completato header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,33 @@
-<header></header>
+<header>
+    <div class="header_top">
+        <div class="container">
+            <div class="header_top_text">
+                <span>DC POWER VISA</span>
+                <a href="#">ADDITIONAL DC SITES<i class="fas fa-sort-down"></i></a>
+                </div>
+            </div>
+    </div>
+    <nav>
+        <div class="container">
+            <div class="header_left">
+                <img src="{{asset('images/dc-logo.png')}}" alt="logo">
+            </div>
+            <div class="header_right">
+                <ul>
+                   <li><a href="#">CHARACTERS</a></li>
+                   <li><a href="#" class="active">COMICS</a></li>
+                   <li><a href="#">MOVIE</a></li>
+                   <li><a href="#">TV</a></li>
+                   <li><a href="#">GAMES</a></li>
+                   <li><a href="#">COLLECTIBLES</a></li>
+                   <li><a href="#">VIDEOS</a></li>
+                   <li><a href="#">FANS</a></li>
+                   <li><a href="#">NEWS</a></li>
+                   <li><a href="#">SHOP</a></li>
+                   <li><a href="#">Search</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="header_hero"></div>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    $data = config('comics');
+    $comics = [
+        'comics' => $data
+    ];
+    return view('index', $comics,);
 });
+
