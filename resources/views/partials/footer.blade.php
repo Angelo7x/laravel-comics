deff87621df7b50b2c1e3d62f563877ed0daaaeb
Angelo7x <abennici96@gmail.com>
2021-11-07 01:01:22 +0100

    fix

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,51 @@
 <footer>
     <div class="footer-top">
       <div class="container">
-        <div class="footer-left"></div>
+        <div class="footer-left">
+          <div class="col-left">
+            <h2>DC COMICS</h2>
+            <ul>
+              <li><a href="#">Characters</a></li>
+              <li><a href="#">Comics</a></li>
+              <li><a href="#">Movies</a></li>
+              <li><a href="#">Tv</a></li>
+              <li><a href="#">Games</a></li>
+              <li><a href="#">Videos</a></li>
+              <li><a href="#">News</a></li>
+            </ul>
+            <h2>SHOP</h2>
+            <ul>
+              <li><a href="#">Shop DC</a></li>
+              <li><a href="#">Shop DC Collectibles</a></li>
+            </ul>
+          </div>
+          <div class="col-middle">
+            <h2>DC</h2>
+            <ul>
+              <li><a href="#">Terms Of Use</a></li>
+              <li><a href="#">Terms Of Use</a></li>
+              <li><a href="#">Terms Of Use</a></li>
+              <li><a href="#">Terms Of Use</a></li>
+              <li><a href="#">Terms Of Use</a></li>
+              <li><a href="#">Terms Of Use</a></li>
+              <li><a href="#">Terms Of Use</a></li>
+              <li><a href="#">Terms Of Use</a></li>
+              <li><a href="#">Terms Of Use</a></li>
+              <li><a href="#">Terms Of Use</a></li>
+              <li><a href="#">Terms Of Use</a></li>
+            </ul>
+          </div>
+          <div class="col-right">
+            <h2>SITES</h2>
+            <ul>
+              <li><a href="#">DC</a></li>
+              <li><a href="#">DC</a></li>
+              <li><a href="#">DC</a></li>
+              <li><a href="#">DC</a></li>
+              <li><a href="#">DC</a></li>
+            </ul>
+          </div>
+        </div>
         <div class="footer-right"></div>
       </div>
     </div>
